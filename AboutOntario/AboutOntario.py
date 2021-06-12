495f717a5ebdec640dc8a82f4f7993a73022a6a2
Zohair Ahmed <Zohair@Zohairs-MacBook-Pro.local>
2021-06-12 18:38:27 -0400

    Added start of MyCity input info

--- AboutOntario/AboutOntario.py
+++ AboutOntario/AboutOntario.py
@@ -2,9 +2,10 @@ from os import closerange
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+import csv
 
 # FUNCTIONS
-def createTable():
+def createTable(): # create csv file of Ontario cities
     url = "https://en.wikipedia.org/wiki/List_of_cities_in_Ontario" #URL
     response = requests.get(url)
 
@@ -17,7 +18,20 @@ def createTable():
     ontDataFrame = pd.DataFrame(ontTable[0]) # convert table to dataframe
     ontDataFrame = ontDataFrame.drop(["Municipalstatus", "Census division"], axis=1) # drop the unwanted columns
     ontDataFrame = ontDataFrame.rename(columns={"Populationdensity": "Population Density"})
-    ontDataFrame.to_csv('PythonProjects/AboutOntario/ontCitiesInfo.csv', index = False) # save to csv file 
+    ontDataFrame.to_csv('PythonProjects/AboutOntario/ontCitiesInfo.csv', index = False) # save to csv file
+
+def aboutMyCity(): # get info about user input city
+    usrCity = input("What is your city?\n")
+
+    with open('PythonProjects/AboutOntario/ontCitiesInfo.csv', 'rt') as f:
+        reader = csv.reader(f, delimiter = ',')
+        for row in reader:
+            if usrCity == row[0]:
+                print(row[0])
+                return
+        print(usrCity + " is not a city in Ontario. Try again!")
+        
+
 
 # MAIN METHOD
 createTable()
@@ -29,6 +43,10 @@ getAction = int(getAction)
 while getAction != 0:
     if getAction == 1:
         print(ontDataFrame)
+    if getAction == 2:
+        aboutMyCity()
     
     getAction = input(actions)
     getAction = int(getAction)  
+
+print("\nSee ya later, eh!")
