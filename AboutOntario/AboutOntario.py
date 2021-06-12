9808d137bb72ec99c6ce4c553ebc031a3d9ae78f
Zohair Ahmed <Zohair@Zohairs-MacBook-Pro.local>
2021-06-11 21:05:13 -0400

    created createTable()

--- AboutOntario/AboutOntario.py
+++ AboutOntario/AboutOntario.py
@@ -3,16 +3,18 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://en.wikipedia.org/wiki/List_of_cities_in_Ontario" #URL
-tableClass = "wikitable sortable jquery-tablesorter"
-response = requests.get(url)
+def createTable():
+    url = "https://en.wikipedia.org/wiki/List_of_cities_in_Ontario" #URL
+    response = requests.get(url)
 
-getPage = BeautifulSoup(response.text, 'html.parser') # get HTML page
-[s.extract() for s in getPage('sup')] # remove superscripts
-getTable = getPage.find('table', {'class': "wikitable"}) # get the table from the HTML page
-ontTable = pd.read_html(str(getTable)) # read the table
+    getPage = BeautifulSoup(response.text, 'html.parser') # get HTML page
+    [s.extract() for s in getPage('sup')] # remove superscripts
+    getTable = getPage.find('table', {'class': "wikitable"}) # get the table from the HTML page
+    ontTable = pd.read_html(str(getTable)) # read the table
 
-ontDataFrame = pd.DataFrame(ontTable[0]) # convert table to dataframe
-ontDataFrame = ontDataFrame.drop(["Municipalstatus", "Census division"], axis=1) # drop the unwanted columns
+    ontDataFrame = pd.DataFrame(ontTable[0]) # convert table to dataframe
+    ontDataFrame = ontDataFrame.drop(["Municipalstatus", "Census division"], axis=1) # drop the unwanted columns
 
-print(ontDataFrame)
+    print(ontDataFrame)
+
+createTable()
