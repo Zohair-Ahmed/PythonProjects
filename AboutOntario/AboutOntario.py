c2f5966e30327e0c96a38d2ae18d83095731c462
Zohair Ahmed <Zohair@Zohairs-MacBook-Pro.local>
2021-06-11 20:54:49 -0400

    removed superscripts

--- AboutOntario/AboutOntario.py
+++ AboutOntario/AboutOntario.py
@@ -8,15 +8,11 @@ tableClass = "wikitable sortable jquery-tablesorter"
 response = requests.get(url)
 
 getPage = BeautifulSoup(response.text, 'html.parser') # get HTML page
+[s.extract() for s in getPage('sup')] # remove superscripts
 getTable = getPage.find('table', {'class': "wikitable"}) # get the table from the HTML page
 ontTable = pd.read_html(str(getTable)) # read the table
-df = pd.DataFrame(ontTable[0]) # convert table to dataframe
 
-# drop the unwanted columns
-data = df.drop([], axis=1)
-# rename columns for ease
-data = data.rename(columns={"Name[1][8]": "Name", "Census division[1][9]": "Census Division", "Population(2016)[2]": "Population (2016)",
-                            "Population(2011)[8]": "Population (2011)", "Change(%)[8]": "Change(%)", "Area(km²)[8]": "Area(km²)", 
-                            "Populationdensity[8]": "Population Density"})
+ontDataFrame = pd.DataFrame(ontTable[0]) # convert table to dataframe
+ontDataFrame = ontDataFrame.drop(["Municipalstatus", "Census division"], axis=1) # drop the unwanted columns
 
-print(data)
+print(ontDataFrame)
