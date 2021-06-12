4792a080af0d3a919e9e0fa5cddc22aec77ba62c
Zohair Ahmed <Zohair@Zohairs-MacBook-Pro.local>
2021-06-12 18:19:31 -0400

    Added user input for number 1 input

--- AboutOntario/AboutOntario.py
+++ AboutOntario/AboutOntario.py
@@ -3,6 +3,7 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
+# FUNCTIONS
 def createTable():
     url = "https://en.wikipedia.org/wiki/List_of_cities_in_Ontario" #URL
     response = requests.get(url)
@@ -12,10 +13,22 @@ def createTable():
     getTable = getPage.find('table', {'class': "wikitable"}) # get the table from the HTML page
     ontTable = pd.read_html(str(getTable)) # read the table
 
+    global ontDataFrame
     ontDataFrame = pd.DataFrame(ontTable[0]) # convert table to dataframe
     ontDataFrame = ontDataFrame.drop(["Municipalstatus", "Census division"], axis=1) # drop the unwanted columns
-    ontDataFrame.to_csv('PythonProjects/AboutOntario/ontCitiesInfo.csv', index = False) # save to csv file
+    ontDataFrame = ontDataFrame.rename(columns={"Populationdensity": "Population Density"})
+    ontDataFrame.to_csv('PythonProjects/AboutOntario/ontCitiesInfo.csv', index = False) # save to csv file 
 
-    print(ontDataFrame)
-
+# MAIN METHOD
 createTable()
+print("\nWelcome to AboutOntario!")
+actions = "\nWhat do you want to know about Ontario?\n\t1 - Everything\n\t2 - My City\n\t3 - Compare Cities\n\t0 - Exit\n"
+getAction = input(actions)
+getAction = int(getAction)
+
+while getAction != 0:
+    if getAction == 1:
+        print(ontDataFrame)
+    
+    getAction = input(actions)
+    getAction = int(getAction)  
